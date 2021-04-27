5007d0ff1c11cbcd73b67a816d199098fbc95084
Aldinonid <aldino.efendi7@gmail.com>
2021-04-27 11:13:18 +0700

    Initial Commit

---
+++ Speakify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Speakify
+//
+//  Created by Aldino Efendi on 27/04/3 R.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
