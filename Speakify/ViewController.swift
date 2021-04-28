eef143b9b51278f3c7261d480a501f4fd5770396
Aldino Efendi <aldino.efendi7@gmail.com>
2021-04-29 02:33:01 +0700

    Finished setup for scenario page and connecting to training page

--- Speakify/ViewController.swift
+++ Speakify/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+			
+			@IBOutlet weak var image1: UIImageView!
+			@IBOutlet weak var image2: UIImageView!
+		
+			@IBOutlet weak var button1: UIButton!
+			@IBOutlet weak var button2: UIButton!
+			
+			
+			override func viewDidLoad() {
+			super.viewDidLoad()
+					button1.layer.cornerRadius = 15
+					self.view.bringSubviewToFront(button1)
+					button2.layer.cornerRadius = 15
+					self.view.bringSubviewToFront(button2)
+					image1.layer.cornerRadius = 15
+					image2.layer.cornerRadius = 15
+					
+					image1.contentMode = .scaleAspectFill
+					image2.contentMode = .scaleAspectFill
 	}
 
-
+	@IBAction func button1Tapped(_ sender: Any) {
+		let vc = storyboard?.instantiateViewController(identifier: "TrainingScreen") as! TrainingViewController
+		
+		vc.isQuestionable = true
+		vc.isInterviewImage = true
+		self.navigationController?.pushViewController(vc, animated: true)
+	}
+	
+	
+	@IBAction func button2Tapped(_ sender: Any) {
+		let vc = storyboard?.instantiateViewController(identifier: "TrainingScreen") as! TrainingViewController
+		
+		vc.isQuestionable = false
+		vc.isInterviewImage = false
+		self.navigationController?.pushViewController(vc, animated: true)
+	}
+	
 }
 
