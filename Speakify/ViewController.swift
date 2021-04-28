708e7757071d660b01386cff6ebc643fdbef372d
Jer1415 <83264179+Jer1415@users.noreply.github.com>
2021-04-28 23:12:48 +0800

    Scenario

--- Speakify/ViewController.swift
+++ Speakify/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
+    @IBOutlet weak var tableview: UITableView!
+    
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+  
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    
+    
+    override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-	}
-
-
+        button1.layer.cornerRadius = 15
+        self.view.bringSubviewToFront(button1)
+        button2.layer.cornerRadius = 15
+        self.view.bringSubviewToFront(button2)
+        image1.layer.cornerRadius = 15
+        image2.layer.cornerRadius = 15
+        
+        image1.contentMode = .scaleAspectFill
+        image2.contentMode = .scaleAspectFill
 }
-
+}
