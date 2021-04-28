f73dcc0dfedae6bc1e0551b016c83b47dbe75006
Aldino Efendi <aldino.efendi7@gmail.com>
2021-04-29 02:53:57 +0700

    Update changes

--- Speakify/ViewController.swift
+++ Speakify/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+<<<<<<< HEAD
 
     @IBOutlet weak var tableview: UITableView!
     
@@ -30,4 +31,44 @@ class ViewController: UIViewController {
         image1.contentMode = .scaleAspectFill
         image2.contentMode = .scaleAspectFill
 }
+=======
+			
+			@IBOutlet weak var image1: UIImageView!
+			@IBOutlet weak var image2: UIImageView!
+		
+			@IBOutlet weak var button1: UIButton!
+			@IBOutlet weak var button2: UIButton!
+			
+			
+			override func viewDidLoad() {
+			super.viewDidLoad()
+					button1.layer.cornerRadius = 15
+					self.view.bringSubviewToFront(button1)
+					button2.layer.cornerRadius = 15
+					self.view.bringSubviewToFront(button2)
+					image1.layer.cornerRadius = 15
+					image2.layer.cornerRadius = 15
+					
+					image1.contentMode = .scaleAspectFill
+					image2.contentMode = .scaleAspectFill
+	}
+
+	@IBAction func button1Tapped(_ sender: Any) {
+		let vc = storyboard?.instantiateViewController(identifier: "TrainingScreen") as! TrainingViewController
+		
+		vc.isQuestionable = true
+		vc.isInterviewImage = true
+		self.navigationController?.pushViewController(vc, animated: true)
+	}
+	
+	
+	@IBAction func button2Tapped(_ sender: Any) {
+		let vc = storyboard?.instantiateViewController(identifier: "TrainingScreen") as! TrainingViewController
+		
+		vc.isQuestionable = false
+		vc.isInterviewImage = false
+		self.navigationController?.pushViewController(vc, animated: true)
+	}
+	
+>>>>>>> new
 }
